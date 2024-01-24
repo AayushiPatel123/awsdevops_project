d08f973d07807ec150e98ed8d432fe891c132a4c
AayushiPatel123 <patel9i5@uwindsor.ca>
2024-01-24 02:05:50 -0500

    adding action pipeline

--- RootLoginAlert/main.tf
+++ RootLoginAlert/main.tf
@@ -3,5 +3,5 @@ module "root_login_alert" {
 
   source                = "./modules/RootLoginAlert/"
   event_name            = "RootLoginActivity"
-  sns_subscription      = ["alvinmanish234@gmail.com"]
+  sns_subscription      = ["aayushipatel0997@gmail.com"]
 }
